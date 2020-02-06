32fa385a2da4ad8aa39c918b9a9bd806b868d9b0
Tanya Kl <tatiana.klenk@gmail.com>
2020-02-06 14:10:23 +0100

    Creating action creators

--- containers/Counter/Counter.js
+++ containers/Counter/Counter.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { connect  } from 'react-redux'
+import * as actionCreators from '../../store/actions'  // import { increment, decrement, add, substract, storeResult, deleteResult } from '../../store/actions'
+
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import { connect  } from 'react-redux'
 import * as actionTypes from '../../store/actions'
 
 class Counter extends Component {
@@ -57,12 +59,12 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
-        onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
-        onAddFiveCounter: () => dispatch({type: actionTypes.ADD, val: 10}), //type: 'HHHH', name: 'dhdh', value: 10 -> cad be added as many properties as you want
-        onSubFiveCounter: () => dispatch({type: actionTypes.SUBSTRACT, val: 15}),
-        onStoreResult: (result) => dispatch({type: actionTypes.STORE_RESULT, result: result}),
-        onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, resultElementId: id}),
+        onIncrementCounter: () => dispatch(actionCreators.increment()),
+        onDecrementCounter: () => dispatch(actionCreators.decrement()),
+        onAddFiveCounter: () => dispatch(actionCreators.add(10)), //type: 'HHHH', name: 'dhdh', value: 10 -> cad be added as many properties as you want
+        onSubFiveCounter: () => dispatch(actionCreators.substract(15)),
+        onStoreResult: (result) => dispatch(actionCreators.storeResult(result)),
+        onDeleteResult: (id) => dispatch(actionCreators.deleteResult(id)),
     }
 }
 
--- store/actions.js
+++ store/actions.js
@@ -4,3 +4,43 @@ export const ADD = 'ADD'
 export const SUBSTRACT = 'SUBSTRACT'
 export const STORE_RESULT = 'STORE_RESULT'
 export const DELETE_RESULT = 'DELETE_RESULT'
+
+export const increment = () => {
+    return {
+        type: INCREMENT
+    }
+}
+
+export const decrement = () => {
+    return {
+        type: DECREMENT
+    }
+}
+
+export const add = (value) => {
+    return {
+        type: ADD,
+        val: value
+    }
+}
+
+export const substract = (value) => {
+    return {
+        type: SUBSTRACT,
+        val: value
+    }
+}
+
+export const storeResult = (res) => {
+    return {
+        type: STORE_RESULT,
+        result: res
+    }
+}
+
+export const deleteResult = (resElId) => {
+    return {
+        type: DELETE_RESULT,
+        resultElementId: resElId
+    }
+}
